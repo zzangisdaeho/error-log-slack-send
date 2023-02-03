e9f795f833a2da1c3107e2645041f176d99f05a2
issac <issac@issacui-MacBookAir.local>
2023-02-03 12:04:40 +0900

    simple

---
+++ src/main/java/com/example/errorlogslacksend/ErrorLogSlackSendApplication.java
@@ -0,0 +1,13 @@
+package com.example.errorlogslacksend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorLogSlackSendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorLogSlackSendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/errorlogslacksend/ErrorLogSlackSendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.errorlogslacksend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorLogSlackSendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
